1b9daac97e7cc36a834595f9b40860d4b7758e85
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-05-19 15:38:30 -0300

    Conectando projeto com banco de dados MongoDB

--- cmd/auction/main.go
+++ cmd/auction/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/joho/godotenv"
+	"github.com/osniantonio/fullcycle-auction-go/configuration/database/mongodb"
 )
 
+// rodar o mongo db via docker
+// docker container run -d -p 27017:27017 --name auctionsDB mongo
+
 func main() {
-	if err := godotenv.Load(); err != nil {
+	ctx := context.Background()
+
+	if err := godotenv.Load("./.env"); err != nil {
+		fmt.Println(err)
 		log.Fatal("Error trying to load env variables")
+		return
+	}
+
+	_, err := mongodb.NewMongoDBConnection(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
 		return
 	}
 }
