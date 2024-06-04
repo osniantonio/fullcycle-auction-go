17d5423b7026a80102ef0b4e542cb9fe393eafcd
Osni Antônio Küchler <osni.antonio@gmail.com>
2024-06-04 20:06:10 -0300

    Criando usecase para busca de maior lance dentro do leilão

--- internal/usecase/auction_usecase/create_auction_usecase.go
+++ internal/usecase/auction_usecase/create_auction_usecase.go
@@ -5,7 +5,9 @@ import (
 	"time"
 
 	"github.com/osniantonio/fullcycle-auction-go/internal/entity/auction_entity"
+	"github.com/osniantonio/fullcycle-auction-go/internal/entity/bid_entity"
 	"github.com/osniantonio/fullcycle-auction-go/internal/internal_error"
+	"github.com/osniantonio/fullcycle-auction-go/internal/usecase/bid_usecase"
 )
 
 type AuctionInputDTO struct {
@@ -25,11 +27,17 @@ type AuctionOutputDTO struct {
 	Timestamp   time.Time        `json:"timestamp"`
 }
 
+type WinningInfoOutputDTO struct {
+	Auction AuctionOutputDTO          `json:"auction"`
+	Bid     *bid_usecase.BidOutputDTO `json:"bid,omitempty"`
+}
+
 type ProductCondition int64
 type AuctionStatus int64
 
 type AuctionUseCase struct {
 	auctionRepositoryInterface auction_entity.AuctionRepositoryInterface
+	bidRepositoryInterface     bid_entity.BidEntityRepository
 }
 
 func (au *AuctionUseCase) CreateAuction(ctx context.Context, auctionInputDTO AuctionInputDTO) *internal_error.InternalError {
